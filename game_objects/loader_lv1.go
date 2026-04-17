7b6ecf77028ce3b5e5adb2590ca02dd419e42e06
pploner <philip@ploner.ch>
2026-04-17 18:20:25 +0200

    added level 1, death obstacles and some player script balance changes

---
+++ game_objects/loader_lv1.go
@@ -0,0 +1,10 @@
+components {
+  id: "game_loader_lv1"
+  component: "/scripts/game_loader_lv1.script"
+}
+embedded_components {
+  id: "level1"
+  type: "collectionproxy"
+  data: "collection: \"/scenes/level1.collection\"\n"
+  ""
+}
